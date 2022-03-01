863e2d1a5767837b11f14ab3b28a1738988c613a
Eric GT <bookjunkie93@gmail.com>
2022-03-01 13:17:28 -0500

    add CORS middleware

--- app/pages/api/helpers/api/api-handler.ts
+++ app/pages/api/helpers/api/api-handler.ts
@@ -1,11 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+import Cors from 'cors'
+
+import initMiddleware from '../../../../lib/init-middleware'
+
 import { errorHandler } from './error-handler'
 
 export { apiHandler }
 
+const cors = initMiddleware(
+  Cors({
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+  })
+)
+
 export default function apiHandler(handler: any) {
+
   return async (req: NextApiRequest, res: NextApiResponse) => {
+    await cors(req, res)
     if(!req.method) return res.status(400).end('No method specified');
     const method = req.method.toLowerCase();
 
